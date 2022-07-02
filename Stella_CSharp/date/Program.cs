ba89ac6a8a7bfb7fa8bae9b84d8fc8876db1fb17
Mateus <mateusteodorovieirabaptista@gmail.com>
2022-07-02 20:04:42 -0300

    Primeiro commit

---
+++ Stella_CSharp/date/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Globalization;
+
+class Program
+{
+    class DateAndTime
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2022, 7, 2);
+            // Console.WriteLine("Date: " + date); // Date: 02/07/2022 00:00:00
+            // Console.WriteLine("Date: " + date.ToString("d")); // Date: 02/07/2022
+            // Console.WriteLine("Date: " + date.ToString("d", new CultureInfo("pt-BR"))); // Date: 02/07/2022
+            // Console.WriteLine("Date: " + date.ToString("dd/MM/yyyy")); // Date: 02/07/2022
+        
+            DateTime time = new DateTime(2022, 7, 2, 13, 14, 15, 987);
+            // Console.WriteLine("Time: " + time); // Time: 02/07/2022 13:14:15
+            // Console.WriteLine("Time: " + time.ToString("HH:mm:ss.fff")); // Time: 13:14:15.987
+
+            // Console.WriteLine("Time: " + time.ToString("D")); // sábado, 2 de julho de 2022
+            // Console.WriteLine("Time: " + time.ToString("m")); // 2 de julho
+            // Console.WriteLine("Time: " + time.ToString("Y")); // julho de 2022
+            // Console.WriteLine("Time: " + time.ToString("G")); // 02/07/2022 13:14:15
+            // Console.WriteLine("Time: " + time.ToString("g")); // 02/07/2022 13:14
+            //Console.WriteLine("Time: " + time.ToString("t")); // 13:14
+            Console.WriteLine("Time: " + time.ToString("T")); // 13:14:15
+
+            //RoadTrip
+            // Console.WriteLine("Time: " + time.ToString("O")); // 2022-07-02T13:14:15.9870000
+            // Console.WriteLine("Time: " + DateTime.Parse(time.ToString("O")).ToString("dd/MM/yyyy HH:mm:ss.fff")); // 02/07/2022 13:14:15.987
+
+        }
+    }
+}
---
+++ Stella_CSharp/validate/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using Caelum.Stella.CSharp.Validation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string cpf = "86288366757";
+        ValidateCPF(cpf);
+
+        string cnpj = "51241758000152";
+        ValidateCNPJ(cnpj);
+
+        string titulo = "041372570132";
+        ValidateTitulo(titulo);
+
+        // Debug.WriteLine(new CPFFormatter().Format(cpfFormatado));
+        // Debug.WriteLine(new CPFFormatter().Unformat(cpfFormatado);
+
+    }
+
+    private static void ValidateTitulo(string titulo)
+    {
+        if (new TituloEleitoralValidator().IsValid(titulo))
+        {
+            Console.WriteLine("Titulo de eleitor válido: " + titulo);
+
+        }
+        else
+        {
+            Console.WriteLine("Titulo de eleitor inválido: " + titulo);
+        }
+    }
+
+    private static void ValidateCNPJ(string cnpg)
+    {
+        if (new CNPJValidator().IsValid(cnpg))
+        {
+            Console.WriteLine("CNPJ válido: " + cnpg);
+
+        }
+        else
+        {
+            Console.WriteLine("CNPJ inválido: " + cnpg);
+        }
+    }
+
+    //Using IsValid
+    private static void ValidateCPF(string cpf)
+    {
+        if (new CPFValidator().IsValid(cpf))
+        {
+            Console.WriteLine("CPF valido: " + cpf);
+        }
+        else
+        {
+            Console.WriteLine("CPF inválido: " + cpf);
+        }
+    }
+
+    //Using AssertValid
+    // private static void ValidateCPF(string cpf)
+    // {
+    //     try
+    //     {
+    //         new CPFValidator().AssertValid(cpf);
+    //         // Debug.WriteLine("CPF valido: " + cpf);
+    //         Console.WriteLine("CPF valido: " + cpf);
+    //     }
+    //     catch (System.Exception)
+    //     {
+    //         // Debug.WriteLine("CPF inválido: " + cpf);
+    //         Console.WriteLine("CPF inválido: " + cpf);
+    //     }
+    // }
+}
