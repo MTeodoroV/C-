d12017304c248cb252ac0a6f8817c445f418ab32
Mateus <mateusteodorovieirabaptista@gmail.com>
2022-07-05 17:13:45 -0300

    Atualizando o projeto checkBank

--- checkBank/ContaCorrent.cs
+++ checkBank/ContaCorrent.cs
@@ -2,12 +2,43 @@ namespace checkBank
 {
     public class contaCorrente
     {
-        public string titular;
         public string conta;
         public int agencia;
         public string nome_agencia;
         public double saldo;
-        public bool verificador;
 
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, contaCorrente destino)
+        {
+            if (saldo < valor || saldo < 0){
+                return false;
+            }
+            else {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
     }
 }
--- checkBank/Program.cs
+++ checkBank/Program.cs
@@ -1,18 +1,44 @@
 ﻿using System;
 using checkBank;
 
+Cliente cliente =  new Cliente();
+cliente.nome = "Mateus";
+cliente.cpf = "123456789";
+cliente.profissao = "Programador";
+
 contaCorrente conta1 = new contaCorrente();
-conta1.titular = "Mateus Teodoro";
 conta1.conta = "1234-x";
 conta1.agencia = 2808;
 conta1.nome_agencia = "Agencia Central";
 conta1.saldo = 100;
 
-Console.WriteLine("Titular: " + conta1.titular);
-Console.WriteLine("Conta: " + conta1.conta);
-Console.WriteLine("Agengia: " + conta1.agencia);
-Console.WriteLine("Nome da agencia: " + conta1.nome_agencia);
-Console.WriteLine("Saldo: R$ " + conta1.saldo + " Reais");
-Console.WriteLine("Vericador: " + conta1.verificador);
+contaCorrente conta2 = new contaCorrente();
+conta2.conta = "1235-x";
+conta2.agencia = 2808;
+conta2.nome_agencia = "Agencia Central";
+conta2.saldo = 50;
+
+// Console.WriteLine("Titular: " + conta1.titular);
+// Console.WriteLine("Conta: " + conta1.conta);
+// Console.WriteLine("Agengia: " + conta1.agencia);
+// Console.WriteLine("Nome da agencia: " + conta1.nome_agencia);
+// Console.WriteLine("Saldo: R$ " + conta1.saldo + " Reais");
+// Console.WriteLine("Vericador: " + conta1.verificador);
+
+Console.WriteLine("Saldo conta 1: " + conta1.saldo);
+Console.WriteLine("Saldo conta 2: " + conta2.saldo);
+bool tranferencia = conta1.Transferir(75, conta2);
+Console.WriteLine("Transferencia: " + tranferencia);
+Console.WriteLine("Saldo conta 1: " + conta1.saldo);
+Console.WriteLine("Saldo conta 2: " + conta2.saldo);
+
+// Console.WriteLine("Saldo pré-saque: " + conta1.saldo);
+// bool saque = conta1.Sacar(10);
+// Console.WriteLine("Saue realizado? " + saque);
+// Console.WriteLine("Saldo pos-saque: " + conta1.saldo);
+
+// Console.WriteLine("Saldo pré-deposito: " + conta1.saldo);
+// conta1.Depositar(50);
+// Console.WriteLine("Saldo pos-deposito: " + conta1.saldo);
 
 Console.ReadKey();
